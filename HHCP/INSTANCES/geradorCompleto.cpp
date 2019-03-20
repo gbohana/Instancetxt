70707eff24617a8d67ed88c829cdf5538c90d1dd
gbohana <gabi_bohana@hotmail.com>
2019-03-20 00:35:01 -0300

    Update geradorCompleto.cpp

--- HHCP/INSTANCES/geradorCompleto.cpp
+++ HHCP/INSTANCES/geradorCompleto.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 // for((i = 1; i<11; i++)); do ./a.out 40 $i; done
-// ./a.out 
+// ./a.out
 pair<double,double> makeCoordenada(){
     pair <double,double> coordenada;
     double r = rand()%31;
@@ -41,23 +41,23 @@ vector<string> split (string s, string delimiter) {
 int main(int argc, char* argv[]){
 
     srand(time(0));
-    
+
     string argv1(argv[1]);
     string argv2(argv[2]);
-    
+
     string output_filename("Instanz_HCSRP_"+argv1+"_"+argv2+".dzn");
     ofstream saida(output_filename.c_str(),ios::out);
     int nbNodes = 12;
     int nbVehi = 3;
     int nbServi = 6;
     int sequencia = atoi(argv[2]);
-    
+
     switch (atoi(argv[1]))
     {
         case 10:
              nbNodes = 12;
              nbVehi = 3;
-             nbServi = 4;            
+             nbServi = 4;
             break;
         case 20:
             nbNodes=22;
@@ -78,9 +78,9 @@ int main(int argc, char* argv[]){
             break;
     }
 
-    saida<<"nbNodes = "<<nbNodes<<";"<<endl; 
-    saida<<"nbVehi = "<<nbVehi<<";"<<endl;   
-    saida<<"nbServi = "<<nbServi<<";"<<endl; 
+    saida<<"nbNodes = "<<nbNodes<<";"<<endl;
+    saida<<"nbVehi = "<<nbVehi<<";"<<endl;
+    saida<<"nbServi = "<<nbServi<<";"<<endl;
     saida<<"numdays = "<<5<<";"<<endl;
 
     int e[5][nbNodes];
@@ -107,7 +107,7 @@ int main(int argc, char* argv[]){
             saida<<setw(5)<< e[i][j]<<",";
         }
         saida<<endl<<" |";
-        
+
     }
     saida<< "];\n"<<endl;
     // Fim da impressao do e
@@ -117,8 +117,8 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < 5; i++)
     {
         for (int j = 0; j < nbNodes; j++)
-            saida<<setw(4)<< l[i][j]<<",";        
-        saida<<endl<<" |";        
+            saida<<setw(4)<< l[i][j]<<",";
+        saida<<endl<<" |";
     }
     saida<< "];\n";
     // Fim da impressao do l
@@ -140,8 +140,8 @@ int main(int argc, char* argv[]){
                 {
                     if(rand()%100 < sequencia*10)
                         a[i][j] = 1;
-                }            
-            }        
+                }
+            }
         }
 
         // Inicio da impressao da tabela a
@@ -149,13 +149,13 @@ int main(int argc, char* argv[]){
         for(int i = 0; i < nbVehi; i++){
             for(int j = 0; j < nbServi; j++)
                 saida<<setw(3)<< a[i][j]<<",";
-            saida<<endl<<" |";        
+            saida<<endl<<" |";
         }
         saida<< "];\n";
 
     // }
 
-    int p[nbNodes][nbVehi][nbServi];    
+    int p[nbNodes][nbVehi][nbServi];
     for(int i = 0; i < nbNodes; i++)
     {
         for(int j = 0; j < nbVehi; j++)
@@ -163,20 +163,20 @@ int main(int argc, char* argv[]){
             for(int k = 0; k < nbServi; k++)
             {
                 if( i == 0 || i == nbNodes-1){
-                    p[i][j][k] = 0;                    
+                    p[i][j][k] = 0;
                 }
                 else
                 {
                     //Calcular a menor janela de tempo - minTimeWindow
                     int minTimeWindow = 480;
                     for(int dia = 0; dia < 5; dia++){
-                        minTimeWindow = min(minTimeWindow,l[dia][i]-e[dia][i]);                        
-                    }                    
-                    p[i][j][k] = min(10+rand()%(int(double(minTimeWindow)*((11-sequencia)/10.0))),minTimeWindow);                    
+                        minTimeWindow = min(minTimeWindow,l[dia][i]-e[dia][i]);
+                    }
+                    p[i][j][k] = min(10+rand()%(int(double(minTimeWindow)*((11-sequencia)/10.0))),minTimeWindow);
                 }
-                
-            }            
-        }        
+
+            }
+        }
     }
 
 
@@ -188,25 +188,25 @@ int main(int argc, char* argv[]){
             for(int k = 0; k < nbServi; k++)
             {
                 saida<<setw(3)<<p[i][j][k]<<",";
-            }      
-            saida<<"\t";      
-        }        
+            }
+            saida<<"\t";
+        }
         saida<<endl;
     }
     saida<< "]);\n";
-	
-	
+
+
 	/***********************     paciente - dia - servico  **************************/
-	
+
 	string rs[nbNodes][nbServi];
 	int minimoPaciente = min(5, nbServi);
-	
+
 	for (int i = 0; i < nbNodes; i++)
 	{
 		for (int k = 0; k < nbServi; k++)
 			{
 				rs[i][k] = "false";
-				
+
 				if (i == 0 || i == nbNodes - 1) {
 					rs[i][k] = "true";
 				}
@@ -216,7 +216,7 @@ int main(int argc, char* argv[]){
     for (int i = 0; i < nbNodes; i++)
 		for (int j = 0; j < 5; j++)
             rs[i][j][rand()%nbServi] = 1;
-			
+
 	saida << "\nrs = \n[|";
 	for (int i = 0; i < nbNodes; i++)
 	{
@@ -236,13 +236,21 @@ int main(int argc, char* argv[]){
     local[0].first = 0;
     local[0].second = 0;
     local[nbNodes-1] = local[0];
-    
+
+    saida<<"coord=[ ";
+    for(int i=0; i < nbNodes; i++){
+            saida<<"("<<local[i].first<<",";
+            saida<<local[i].second<<"), ";
+    }
+    saida<< "];\n"<<endl;
+
+
     for(int i=0; i < nbNodes; i++){
         for(int j=0; j < nbNodes; j++){
             matriz[i][j] = distancia(local[i],local[j]);
         }
     }
-    
+
     saida<<"d=[";
     for(int i=0; i < nbNodes; i++){
         saida<<"|";
