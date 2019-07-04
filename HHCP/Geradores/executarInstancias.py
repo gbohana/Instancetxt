5740db0efa2c830444148c872ebb29bae3c3c431
gbohana <gabi_bohana@hotmail.com>
2019-07-04 13:59:59 -0300

    execurtarinstancias pronto (?)

--- HHCP/Geradores/executarInstancias.py
+++ HHCP/Geradores/executarInstancias.py
@@ -11,44 +11,17 @@ output_file = '/home/gabriela/NurseRostering/HHCP/Geradores/res.csv'
 tmp_log_file = '/home/gabriela/NurseRostering/HHCP/Geradores/tmp_log_file.txt'
 path_to_mzn = '/home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn'
 
-def get_dist( output ):
-    pattern = 'Distancia = (.*)\n'
-    m = re.search( pattern, output )
-    dist = m.group( 1 )
-    return dist
-
-
-def get_vis( output ):
-    pattern = 'Visitas = (.*)\n'
-    m = re.search( pattern, output )
-    vis = m.group( 1 )
-    return vis
-
-o_f = open( output_file, 'wb' )
-writer = csv.writer( o_f )
-writer.writerow( [ 'visitas', 'distancias' ] )
+# o_f = open( output_file, 'wb' )
+# writer = csv.writer( o_f )
+# writer.writerow( [ 'visitas', 'distancias' ] )
 
 for filename in os.listdir(path_to_dzn):
     # executes program passing files as parameters
-    # subprocess.call([cmd, filename])
-    cmd = '/home/gabriela/MiniZincIDE-2.2.3-bundle-linux/MiniZincIDE.sh  {} {} | tee {}'.format( path_to_mzn, filename, tmp_log_file )
+    # # subprocess.call([cmd, filename])
+    arq = filename
+    if arq.endswith('.dzn'):
+        arq = arq[:-4]
+    cmd = './minizinc --solver Gurobi /home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn /home/gabriela/INSTANCES\ FINAL/' + filename +' -o /home/gabriela/INSTANCES\ FINAL/' + arq + '.sol'
     os.system( cmd )
-
-    output = open( tmp_log_file, 'r' ).read()
-
-    # get data from output
-    vis = get_dist(output)
-    dist = get_vis(output)
-
-    # writes filtered results in csv file
-    writer.writerow( [ vis, dist ] )
-    o_f.flush()
-
-# ==> colocar gurobi nos parametros pra ver se vai
-# File "executarInstancias.py", line 41, in <module>
-#     vis = get_dist(output)
-#   File "executarInstancias.py", line 18, in get_dist
-#     dist = m.group( 1 )
-# AttributeError: 'NoneType' object has no attribute 'group'
 
 #./minizinc --solver Gurobi ~/NurseRostering/HHCP/HCS_Novo.mzn ~/INSTANCES\ FINAL/Instanz_HCSRP_3_10_0_0.dzn -o saida.txt
