4a4eb78bc5dfcd7fa5a04bb734afc6994fafc39d
gbohana <gabi_bohana@hotmail.com>
2019-07-04 14:13:35 -0300

    Update executarInstancias.py

--- HHCP/Geradores/executarInstancias.py
+++ HHCP/Geradores/executarInstancias.py
@@ -7,13 +7,7 @@ import subprocess
 import os
 
 path_to_dzn = '/home/gabriela/INSTANCES FINAL/'
-output_file = '/home/gabriela/NurseRostering/HHCP/Geradores/res.csv'
-tmp_log_file = '/home/gabriela/NurseRostering/HHCP/Geradores/tmp_log_file.txt'
 path_to_mzn = '/home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn'
-
-# o_f = open( output_file, 'wb' )
-# writer = csv.writer( o_f )
-# writer.writerow( [ 'visitas', 'distancias' ] )
 
 for filename in os.listdir(path_to_dzn):
     # executes program passing files as parameters
@@ -21,6 +15,7 @@ for filename in os.listdir(path_to_dzn):
     arq = filename
     if arq.endswith('.dzn'):
         arq = arq[:-4]
+    print(arq + " "+ filename)
     cmd = './minizinc --solver Gurobi /home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn /home/gabriela/INSTANCES\ FINAL/' + filename +' -o /home/gabriela/INSTANCES\ FINAL/' + arq + '.sol'
     os.system( cmd )
 
