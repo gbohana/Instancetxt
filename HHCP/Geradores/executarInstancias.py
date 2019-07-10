446f882c9b121019526ee66d062579e984f24b77
gbohana <gabi_bohana@hotmail.com>
2019-07-10 12:08:30 -0300

    Update executarInstancias.py

--- HHCP/Geradores/executarInstancias.py
+++ HHCP/Geradores/executarInstancias.py
@@ -5,18 +5,21 @@ import re
 import csv
 import subprocess
 import os
+import os.path
 
 path_to_dzn = '/home/gabriela/INSTANCES FINAL/'
 path_to_mzn = '/home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn'
 
 for filename in os.listdir(path_to_dzn):
     # executes program passing files as parameters
-    # # subprocess.call([cmd, filename])
     arq = filename
     if arq.endswith('.dzn'):
         arq = arq[:-4]
-    #print(arq + " "+ filename)
-    cmd = './minizinc --solver Gurobi /home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn /home/gabriela/INSTANCES\ FINAL/' + filename +' -o /home/gabriela/INSTANCES\ FINAL/' + arq + '.sol'
-    os.system( cmd )
+        arqcheck = arq + '.sol'
+        print(arq + ' '+ arqcheck)
+        print( str(os.path.exists(arqcheck) )) #ta dando false, qd deveria ser true; com './' na frente tb
+        if os.path.exists('./' +arqcheck)==0:
+            cmd = './minizinc --solver Gurobi /home/gabriela/NurseRostering/HHCP/HCS_Novo.mzn /home/gabriela/INSTANCES\ FINAL/' + filename +' -o /home/gabriela/INSTANCES\ FINAL/' + arq + '.sol'
+            os.system( cmd )
 
 #./minizinc --solver Gurobi ~/NurseRostering/HHCP/HCS_Novo.mzn ~/INSTANCES\ FINAL/Instanz_HCSRP_3_10_0_0.dzn -o saida.txt
