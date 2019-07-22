c1f62cae7fe28dfae029b7573f2f41f519b7b6b9
gbohana <gabi_bohana@hotmail.com>
2019-07-22 13:39:44 -0300

    gerarinst funcionando

--- HHCP/Geradores/gerarInstancias.py
+++ HHCP/Geradores/gerarInstancias.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import time
 
 #for loop pra executar (3 x 3 x 11) x 30 vezes
 
@@ -10,5 +11,5 @@ with open('argumentos.txt','r') as f:
         for x in range(30):
             l = line.split()
             l.append(str(x))
+            time.sleep(1)
             subprocess.Popen([program]+l)
-
